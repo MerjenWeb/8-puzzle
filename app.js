d9d85a81909b04e539b4d4dfb3195af088510944
Merjen <merjengarajaeva@yandex.ru>
2023-09-03 22:22:50 +0500

    Structured landing page

---
+++ app.js
@@ -0,0 +1,6 @@
+'use strict';
+const landingPage = document.querySelector('.landing-page');
+console.log(landingPage);
+const slider = document.querySelectorAll('.slider');
+slider.forEach(slide => console.log(slide));
+//opacity, smooth transition between the slider divs
